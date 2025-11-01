da81580101761a51fc92f4c02b67f193e8061d93
Yuri Seki <yuriseki@gmail.com>
2025-11-01 17:27:51 -0300

    Left sidebar

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from "react";
 import Navbar from "@/app/components/navigation/navbar";
+import LeftSidebar from "@/app/components/navigation/LeftSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+      <div className="flex">
+        <LeftSidebar />
+        <section className="max-md:pd-14 flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
